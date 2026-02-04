139ca606de24a41477a36e7a5284f95d5c7b49bd
Santosh <gvsantosh.05@gmail.com>
2026-02-04 10:05:59 +0530

    Update code files

--- binsub.go
+++ binsub.go
@@ -3,5 +3,19 @@ package main
 import ()
 
 func binSub() {
+	nums := []int{1, 0, 1, 0, 1}
+	c := 0
+	hash := make(map[int]int)
+	k := 2
+	hash[0] = 1
+	prefSum := 0
+	for i := 0; i < len(nums); i++ {
+		prefSum += nums[i]
+		goal := prefSum - k
+		if freq, exists := hash[goal]; exists {
+			c += freq
+		}
+		hash[prefSum]++
+	}
 
 }
